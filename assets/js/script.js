1bc27f4f745d6f138e30ee99055d12925c86b6ba
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-14 17:41:16 -0400

    fixed time block function to display correct past present and future styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,27 @@
-var divLimit = 17;
 $(document).ready(function() {
+    var divLimit = 17;
     for (var i = 9; i <= divLimit; i++) {
         $(".container").append("<div class='row time-block' id='time" + i + "'></div>");
 
     };
     
     $(".time-block").append("<div class='col-1 hour'></div>");
+    for (var i = 9; i <= divLimit; i++) {
 
-            // var morning = i + "AM";
-            // var afternoon = i + "PM";
-            // if (i <= 11) {
-            //     console.log("morning");
-            //     $(".hour").html(morning);
-            // } else {
-            //     console.log("afternoon");
-            //     $(".hour").html(afternoon);
-            // }
+        var morning = i + "AM";
+        var afternoon = i + "PM";
 
+        $(".hour").text(morning);
+    }
     $(".time-block").append("<textarea class='col-10 description'></textarea>");
     $(".time-block").append("<button class='col-1 saveBtn'></button>");
     $(".saveBtn").append("<i class='fas fa-save'></i>");
 
+    
     var getCurrentTime = moment().hour();
-    console.log(getCurrentTime);
-
     function singleTimeBlock() {
         $(".time-block").each(function() {
-            var blockTime = parseInt($(this).attr("id").split()[1]);
+            var blockTime = parseInt($(this).attr("id").split("time")[1]);
             console.log(blockTime);
             if (blockTime === getCurrentTime) {
                 $(this).children(".description").addClass("present");
