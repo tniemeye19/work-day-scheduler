e96d52af0c38c2330cde0dad727c1e675a6e6d68
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-14 16:24:03 -0400

    updated loop, now adds unique id to time-block classed divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var divLimit = 9;
+var divLimit = 17;
 $(document).ready(function() {
-    for (var i = 1; i <= divLimit; i++) {
+    for (var i = 9; i <= divLimit; i++) {
         $(".container").append("<div class='row time-block'></div>");
+        $(".time-block").each(function (i) {
+            $(this).attr("id", "time" + i);
+        });
+    };
+    
+    
 
-        
-    }
-    // $(".time-block").attr("id", "time" + [i]);
     $(".time-block").append("<div class='col-1 hour'></div>");
     $(".time-block").append("<textarea class='col-10 description'></textarea>");
     $(".time-block").append("<button class='col-1 saveBtn'></button>");
